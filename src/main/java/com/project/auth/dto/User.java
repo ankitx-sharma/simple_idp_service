8730699cececeb9d7e2aac3894f328d8c23f50fb
Ankita Sharma <ankitasharma.281993@gmail.com>
2026-01-11 18:11:25 +0100

    Updated -> added role based changes in dto and service file. Added new service methods for admin calls

--- src/main/java/com/project/auth/dto/User.java
+++ src/main/java/com/project/auth/dto/User.java
@@ -1,24 +1,28 @@
 package com.project.auth.dto;
 
+import com.project.auth.entity.Role;
 import com.project.auth.entity.UserEntity;
 
 public class User {
 	private long id;
 	private String username;
 	private String password;
+	private Role role;
 	
 	public User() { super(); }
 	
-	public User(long id, String username, String password) {
+	public User(long id, String username, String password, Role role) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
+		this.role = role;
 	}
 
 	public User(UserEntity entity) {
 		this.id = entity.getId();
 		this.username = entity.getUsername();
 		this.password = entity.getPassword();
+		this.role = entity.getRole();
 	}
 
 	public long getId() {
@@ -43,5 +47,13 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public Role getRole() {
+		return this.role;
+	}
+	
+	public void setRole(Role role) {
+		this.role = role;
 	}
 }
--- src/main/java/com/project/auth/service/UserService.java
+++ src/main/java/com/project/auth/service/UserService.java
@@ -7,11 +7,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.project.auth.dto.User;
+import com.project.auth.entity.Role;
 import com.project.auth.entity.UserEntity;
 import com.project.auth.repository.UserRepository;
+import com.project.auth.util.JwtUtil;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private JwtUtil jwtUtil;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -21,8 +26,25 @@ public class UserService {
 	
 	public void registerUser(User user) {
 		String password = passwordEncoder.encode(user.getPassword());
-		UserEntity userEntity = new UserEntity(user.getUsername(), password);
+		UserEntity userEntity = new UserEntity(user.getUsername(), password, Role.USER);
 		userRepository.save(userEntity);
+	}
+	
+	public void registerAdmin(User user) {
+		String password = passwordEncoder.encode(user.getPassword());
+		UserEntity userEntity = new UserEntity(user.getUsername(), password, Role.ADMIN);
+		userRepository.save(userEntity);
+	}
+	
+	public String loginUser(User loginRequest) {
+		User user = findByUserName(loginRequest.getUsername())
+				.orElseThrow(() -> new RuntimeException("Invalid credentials"));
+		
+		if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+		    throw new RuntimeException("Invalid credentials");
+		}
+		
+		return jwtUtil.generateAccessToken(loginRequest.getUsername(), user.getRole());
 	}
 	
 	public Optional<User> findByUserName(String username){
